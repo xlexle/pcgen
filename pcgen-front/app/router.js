52bbff80e5586293c85fb1dc205f58eba6f89f74
xlexle <lehtinenv@gmail.com>
2017-02-20 18:31:30 +0200

    templating

--- pcgen-front/app/router.js
+++ pcgen-front/app/router.js
@@ -8,17 +8,17 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login', {path: '/kirjaudu'});
-  // this.route('docs', {path: '/ohjeet'}, function() {
-  //   this.route('parts', { path: '/komponentit' });
-  //   this.route('peripherals', { path: '/oheislaitteet' });
-  //   this.route('assembly', { path: '/kasaus' });
-  // });
   this.route('parts', { path: '/komponentit' }, function() {
     this.route('power-supplies', { path: '/virtalahteet' });
     this.route('fans', { path: '/tuulettimet' });
   });
   // this.route('peripherals', {path: '/oheislaitteet'}, function() {
   //   this.route('monitors', { path: '/naytot' });
+  // });
+  // this.route('docs', {path: '/ohjeet'}, function() {
+  //   this.route('parts', { path: '/komponentit' });
+  //   this.route('peripherals', { path: '/oheislaitteet' });
+  //   this.route('assembly', { path: '/kasaus' });
   // });
 });
 
