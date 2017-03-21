9b0133512cbb014da9aa57f15bf83bf8d8a807fb
xlexle <lehtinenv@gmail.com>
2017-03-21 10:58:03 +0200

    json correct both for GET and POST

--- pcgen-front/app/serializers/application.js
+++ pcgen-front/app/serializers/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
-const { String: { singularize, camelize, underscore, capitalize } } = Ember;
+const { String: { singularize, camelize, dasherize, underscore, capitalize } } = Ember;
 
 export default DS.JSONAPISerializer.extend({
 
@@ -12,11 +12,44 @@ export default DS.JSONAPISerializer.extend({
     return converted;
   },
 
-  keyForAttribute(attr) {
-    return underscore(attr);
+  // serialize: function(snapshot, options) {
+  //   var json = {};
+  //
+  //   snapshot.eachAttribute(function(name) {
+  //     json[underscore(name)] = snapshot.attr(name);
+  //   })
+  //
+  //   snapshot.eachRelationship(function(name, relationship) {
+  //     json[underscore(name)] = snapshot.
+  //     if (relationship.kind === 'hasMany') {
+  //       json[serverHasManyName(name)] = snapshot.hasMany(name, { ids: true });
+  //     }
+  //
+  //
+  //   });
+  //
+  //   if (options.includeId) {
+  //     json.ID_ = snapshot.id;
+  //   }
+  //
+  //   return json;
+  // }
+
+  // Converts "attribute-key" to "attribute_key" in PUT/POST requests
+  keyForAttribute(attr, method) {
+    if (method === "serialize") {
+      return underscore(attr);
+    } else {
+      return dasherize(attr);
+    }
   },
 
-  keyForRelationship: function(attr) {
-    return underscore(attr);
+  // Converts "relationship-key" to "relationship_key" in PUT/POST requests
+  keyForRelationship: function(key, relationship, method) {
+    if (method === "serialize") {
+      return underscore(key);
+    } else {
+      return dasherize(key);
+    }
   }
 });
