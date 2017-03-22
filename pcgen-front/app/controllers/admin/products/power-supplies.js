2333e4d074fb343a0c6fc9e0f40eb5182047844a
xlexle <lehtinenv@gmail.com>
2017-03-22 22:04:18 +0200

    modal combined with dynamicproduct route

--- pcgen-front/app/controllers/admin/products/power-supplies.js
+++ pcgen-front/app/controllers/admin/products/power-supplies.js
@@ -3,3 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   count: Ember.computed.alias('model.length')
 });
+
+Ember.LinkComponent.reopen({
+  attributeBindings: ['data-toggle', 'data-target']
+});
