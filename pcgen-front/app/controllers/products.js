9dece20d117c59ff10b1fe198747089feb1ae1f4
xlexle <lehtinenv@gmail.com>
2017-03-28 17:15:33 +0300

    refactoring and minor css changes

--- pcgen-front/app/controllers/products.js
+++ pcgen-front/app/controllers/products.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  classNames: ['row', 'products-area'],
+  isAdmin: true,
+  showComponentCategories: true,
+  showPeripheralCategories: false,
+
+  actions: {
+    selectComponents() {
+      this.set('showComponentCategories', true);
+      this.set('showPeripheralCategories', false);
+    },
+
+    selectPeripherals() {
+      this.set('showComponentCategories', false);
+      this.set('showPeripheralCategories', true);
+    }
+  }
 });
